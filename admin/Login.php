e11d687fb107f3af15ce5efa86cf6d32955c475a
bazirake <bazirakeric1@gmail.com>
2022-09-01 19:04:02 +0200

    admin changes

--- admin/Login.php
+++ admin/Login.php
@@ -18,23 +18,31 @@
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
 </head>
 
-<body class="bg-gradient-primary">
+<body class="bg-gradient-primary" >
 <div class="container">
     <!-- Outer Row -->
     <div class="row justify-content-center">
         <div class="col-xl-10 col-lg-12 col-md-9">
-            <div class="card o-hidden border-0 shadow-lg my-5">
+            <div class="card  o-hidden border-0 shadow-lg my-5"
+            style="border-top-left-radius: 60px!important;
+            border-bottom-left-radius: 57px!important;
+">
                 <div class="card-body p-0">
                     <!-- Nested Row within Card Body -->
                     <div class="row">
-                        <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                        <div class="col-lg-6 d-none d-lg-block "
+                        style="background:url(
+                        http://www.peaceforthestorm.com/wp-content/uploads/2016/04/The-Holy-Bible.png);
+                        background-position:center;
+                        background-size:cover">
+                        </div>
                         <div class="col-lg-6">
-                            <div class="p-5">
-                                <div class="text-center">
-                                    <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
-                                </div>
-                                <form class="user">
-                                    <div class="form-group">
+                          <div class="p-5">
+                            <div class="text-center">
+                              <h1 class="h4 text-gray-900 mb-4">Scripture Union Admin</h1>
+                            </div>
+                             <form class="user">
+                                <div class="form-group">
                                      <input type="email" class="form-control form-control-user"
                                         id="exampleInputEmail" aria-describedby="emailHelp"
                                           placeholder="Enter Email Address...">
@@ -45,7 +53,7 @@
                                     </div>
                                     <div class="form-group">
                                         <div class="custom-control custom-checkbox small">
-                                            <input type="checkbox" class="custom-control-input" id="customCheck">
+                                          <input type="checkbox" class="custom-control-input" id="customCheck">
                                             <label class="custom-control-label" for="customCheck">Remember
                                                 Me</label>
                                         </div>
@@ -54,10 +62,10 @@
                                       Login
                                     </a>
                                     <hr>
-                                    <a href="index.php" class="btn btn-google btn-user btn-block">
+                                    <a href="index.php" class="btn btn-google btn-user ">
                                       <i class="fab fa-google fa-fw"></i> Login with Google
                                     </a>
-                                    <a href="index.php" class="btn btn-facebook btn-user btn-block">
+                                    <a href="index.php" class="btn btn-facebook btn-user ">
                                       <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
                                     </a>
                                 </form>
@@ -65,9 +73,7 @@
                                 <div class="text-center">
                                     <a class="small" href="forgot-password.html">Forgot Password?</a>
                                 </div>
-                                <div class="text-center">
-                                    <a class="small" href="register.html">Create an Account!</a>
-                                </div>
+
                             </div>
                         </div>
                     </div>
